885042b9471cc40fd0118f074276aa9e709cc68f
RyanShaw21 <>
2021-05-13 16:38:10 +0000

    Fixed formatting

--- fahrenheitToCelsius.go
+++ fahrenheitToCelsius.go
@@ -1,7 +1,7 @@
 // Created by: Ryan-Shaw-2
 // Created on: May 2021
 //
-// This program coverts fahrenheit to celsius
+// This program converts fahrenheit to celsius
 
 package main
 
@@ -22,7 +22,7 @@ func main() {
 	fmt.Println()
 
 	// process
-	var celcius = (fahrenheit - 32) * (5.0/9.0)
+	var celcius = (fahrenheit - 32) * (5.0 / 9.0)
 
 	// output
 	fmt.Println(fahrenheit, "°F is", math.Round(celcius*1000)/1000, "°C")
